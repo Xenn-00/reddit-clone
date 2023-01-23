334088b2ed5ebb66cd4f75338344a7c3e5910dad
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-23 11:41:33 +0700

    auth modal

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,64 @@
+import { authModelState } from "@/atoms/authModalAtoms";
+import {
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
+import OAtuhButtons from "./OAtuhButtons";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModelState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign={"center"}>
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            pb={6}
+          >
+            <Flex
+              direction={"column"}
+              align={"center"}
+              justify={"center"}
+              width={"70%"}
+            >
+              <OAtuhButtons />
+              <Text color={"gray.400"} fontWeight={700} fontSize={"10pt"}>
+                OR
+              </Text>
+              <AuthInputs />
+              {/* <ResetPassword /> */}
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
