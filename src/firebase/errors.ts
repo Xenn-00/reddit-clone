cacaa72789fed2bd891cff276b348bd9812da1e2
Xenn-00 <node.xenon@gmail.com>
2023-01-29 15:48:01 +0700

    errors

---
+++ src/firebase/errors.ts
@@ -0,0 +1,5 @@
+export const FIREBASE_ERRORS = {
+  "Firebase: Error (auth/email-already-in-use).": "Email already in use",
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+};
